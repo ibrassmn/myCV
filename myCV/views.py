fb77c002530514721d979a129135ba0fd52bca7a
ibrassmn <ibrassmn@gmail.com>
2024-05-13 22:26:21 +0300

    some news

--- myCV/views.py
+++ myCV/views.py
@@ -7,7 +7,17 @@ def index(request):
     site_description = GeneralSetting.objects.get(name="site_description").parameter
     home_banner_name = GeneralSetting.objects.get(name="home_banner_name").parameter
     about_myself_footer = GeneralSetting.objects.get(name="about_myself_footer").parameter
+    about_myself_title = GeneralSetting.objects.get(name="about_myself_title").parameter
     about_myself_about_section = GeneralSetting.objects.get(name="about_myself_about_section").parameter
+    about_myself_about_section_1 = GeneralSetting.objects.get(name="about_myself_about_section_1").parameter
+    about_myself_about_section_long = GeneralSetting.objects.get(name="about_myself_about_section_long").parameter
+    skills_section = GeneralSetting.objects.get(name="skills_section").parameter
+    resume_section_0 = GeneralSetting.objects.get(name="resume_section_0").parameter
+    certificate_section = GeneralSetting.objects.get(name="certificate_section").parameter
+
+
+
+
 
     context = {
         'site_title': site_title,
@@ -16,6 +26,16 @@ def index(request):
         'home_banner_name': home_banner_name,
         'about_myself_footer': about_myself_footer,
         'about_myself_about_section': about_myself_about_section,
+        'about_myself_about_section_1': about_myself_about_section_1,
+        'about_myself_about_section_long': about_myself_about_section_long,
+        'about_myself_title': about_myself_title,
+        'skills_section': skills_section,
+        'resume_section_0': resume_section_0,
+        'certificate_section': certificate_section,
+
+
+
+
 
 
 
