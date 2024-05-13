45a62f66e6a582471f83c5f6b4741564ff458e83
ibrassmn <ibrassmn@gmail.com>
2024-05-13 22:24:30 +0300

    feat: create image settings model

--- myCV/models.py
+++ myCV/models.py
@@ -18,7 +18,7 @@ class GeneralSetting(models.Model):
     )
     parameter = models.CharField(
         default='',
-        max_length=254,
+        max_length=20000,
         blank=True,
         verbose_name="Parameter",
         help_text='',
@@ -40,4 +40,45 @@ class GeneralSetting(models.Model):
     class Meta:
         verbose_name = 'General Setting'
         verbose_name_plural = 'General Settings'
+        ordering = ('name',)
+
+class ImageSetting(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,  # boş veriyi kaydetmeyi sağlar
+        verbose_name="Name",
+        help_text='The name of the general setting',
+    )
+    description =models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name="Description",
+        help_text='The description of the general setting',
+    )
+    file = models.FileField(
+        default='',
+        verbose_name='Image',
+        help_text='',
+        blank=True,
+        upload_to='images/',
+    )
+    updated_date = models.DateTimeField(
+        blank=True,
+        auto_now = True,
+        verbose_name="Updated Date",
+    )
+    created_date = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name='Created Date',
+    )
+
+    def __str__(self):
+        return f'Image Setting: {self.name}'
+
+    class Meta:
+        verbose_name = 'Image Setting'
+        verbose_name_plural = 'Image Settings'
         ordering = ('name',)
